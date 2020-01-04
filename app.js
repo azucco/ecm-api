b867c0f7d09d6d2a325997b58ba2f78b77c74bb3
azucco <alessandro.zucco@gmail.com>
2020-01-04 11:29:39 +0100

    cors

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ app.use('/coffee', (req, res, next) =>{
 });
 
 // CORS
-app.use(cors())
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 
 
 // === Routes 
